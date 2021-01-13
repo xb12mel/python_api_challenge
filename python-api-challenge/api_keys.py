1a624ccefe938a02d77abf5336f776134925e80b
Melvin Byrd <xb12mel@gmail.com>
2021-01-12 23:14:07 -0500

    Update api_keys.py
    removed personal keys from file

--- python-api-challenge/api_keys.py
+++ python-api-challenge/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "1116e30a0edbcd04aac8bb588ec50f32"
+weather_api_key = "Your Key Here"
 
 # Google API Key
-g_key = "AIzaSyAuwSHh4qNVhWnfMv4egsE0NhlIExCBJyM"
+g_key = "Your Key Here"
